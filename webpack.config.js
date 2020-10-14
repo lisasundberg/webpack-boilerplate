5304bd50bc1a329379d56c90d7257f417f3e8059
Lisa <ls@anotherstate.co>
2020-10-14 16:03:06 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './app/index.js',
+	module: {
+		rules: [
+			{ test: /\.svg$/, use: 'svg-inline-loader' },
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+			{ test: /\.(js)$/, use: 'babel-loader' }
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin(),
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: 'production'
+		})
+	],
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
